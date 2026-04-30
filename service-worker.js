de051dc606c99247141bedda0b6b573df519d265
minezoid02-sketch <minezoid02@gmail.com>
2026-04-30 11:08:16 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "kana-cache-v1";
+const URLS_TO_CACHE = [
+  "/kana/",
+  "/kana/index.html"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
